1b1fba42f8c935cb5d6d992544035f6578d9c8ad
Arvend Karunagaran <79296644+ArvendK21667@users.noreply.github.com>
2021-02-26 13:02:33 +1100

    Update 2

--- Assets/FallingStones.cs
+++ Assets/FallingStones.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class FallingStones : MonoBehaviour
 {
+    public GameObject rockboi;
+    public GameObject RockSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        RockSpawn = GameObject.Find("RockSpawn");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(rockboi, new Vector3(0, 5, 0), Quaternion.identity);
+        }
     }
 
     public Transform gameobject;
